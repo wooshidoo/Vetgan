9705512e2e73384d3f68237d4644530cdce67e5b
elias <eliasariaslazo@gmail.com>
2024-10-14 14:50:35 -0300

    CAMBIOS DISEÑO INTERFACES Y AGREGUÉ SPINNER

--- app/src/main/java/com/example/vetgan/registrar_animal.java
+++ app/src/main/java/com/example/vetgan/registrar_animal.java
@@ -1,6 +1,9 @@
 package com.example.vetgan;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Spinner;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +11,10 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.lang.reflect.Array;
+
 public class registrar_animal extends AppCompatActivity {
+    Spinner comboanimales;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +26,9 @@ public class registrar_animal extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        comboanimales = (Spinner) findViewById(R.id.idSpinnerAnimal);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.combo_animales, android.R.layout.simple_spinner_item);
+        comboanimales.setAdapter(adapter);
     }
 }
